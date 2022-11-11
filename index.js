bed2368c3533d34665b18d1be10577df9825f7d0
Lisa Meyer <lisamermey@gmail.com>
2022-11-11 16:47:40 -0500

    Create express server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+server.listen(3000, () => {
+  console.log(`
+  🔉 listening on *: 3000
+  🚀 server on http://localhost:3000
+  `);
+});
